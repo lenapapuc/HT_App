0e5b9d5df6c73fe093223910ceb8c39091d1b6bd
lenapapuc <elenapapuc99@gmail.com>
2024-06-07 01:17:28 +0300

    Changes

--- Shared/MessageDtoPost.cs
+++ Shared/MessageDtoPost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
@@ -12,6 +13,9 @@ namespace Shared
         public string Content { get; set; }
         public string IntendedFor { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string UserId { get; set; }  
+        public string UserId { get; set; }
+
+   
+
     }
 }
