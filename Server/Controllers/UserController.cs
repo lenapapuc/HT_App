133d4037b15227217820e4a58c1b48db68fb21a7
lenapapuc <elenapapuc99@gmail.com>
2024-06-06 10:16:09 +0300

    Changes

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Domain;
+using Infrastructure;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using System.Net;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _userManager = userManager;
+        }
+
+       
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<UserDto>> GetUserById(Guid id)
+        {
+            var user = await _userManager.FindByIdAsync(id.ToString());
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+
+
+            var userDto = new UserDto
+            {
+                Name = user.Name,
+                Strikes = user.Strikes,
+                Role = roles[0]
+            };
+
+            return Ok(userDto);
+        }
+    }
+}
