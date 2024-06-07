5abedddd412611d0ed1d7c248476e2a19494a75d
unknown <catalintin00@gmail.com>
2024-06-07 05:36:56 +0300

    final push

---
+++ Domain/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class NewsDomain
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string ShortDescription { get; set; }
+        public string Link { get; set; }
+    }
+}
